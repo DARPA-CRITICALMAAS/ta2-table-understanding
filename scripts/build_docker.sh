52fed17255433d890b806958ddc62efc5a1ec17f
Binh Vu <binh@toan2.com>
2025-10-10 23:13:55 +0700

    change git from ssh to http protocol as server block ssh

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-export DATA_REPO_COMMIT_ID=$(git ls-remote "git@github.com:DARPA-CRITICALMAAS/ta2-minmod-data.git" "refs/heads/main" | awk '{print $1}')
-export KG_REPO_COMMIT_ID=$(git ls-remote "git@github.com:DARPA-CRITICALMAAS/ta2-minmod-kg.git" "refs/heads/main" | awk '{print $1}')
+export DATA_REPO_COMMIT_ID=$(git ls-remote "https://github.com/DARPA-CRITICALMAAS/ta2-minmod-data.git" "refs/heads/main" | awk '{print $1}')
+export KG_REPO_COMMIT_ID=$(git ls-remote "https://github.com/DARPA-CRITICALMAAS/ta2-minmod-kg.git" "refs/heads/main" | awk '{print $1}')
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
